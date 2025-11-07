f4165a98aa968bdfc592f0ca2ff7d04f0de0707e
Udaysuman <udaysuman16@gmail.com>
2025-11-07 10:18:26 +0530

    Reinitialize and push properly from KissanBazzar-deployment

--- KissanBazzar-deployment/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['newsapi.org', 'www.freep.com'], // Add any other domains you use
-  },
-};
-
-export default nextConfig;
